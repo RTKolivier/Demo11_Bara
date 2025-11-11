91a31afe3ec061e1e005912c8d94b3386e730b35
RTKolivier <rtkolivier@gmail.com>
2025-11-11 13:43:46 +0300

    first commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Barashikhina_Sofia
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
